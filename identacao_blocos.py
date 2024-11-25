22e47d1115e025647999bcc1eeb2728d12e70944
Alan Moura Santana <alan.moura2@hotmail.com>
2024-11-25 20:59:07 -0300

    identacao

---
+++ identacao_blocos.py
@@ -0,0 +1,13 @@
+def sacar (valor):
+    saldo = 500
+
+
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire o seu dinheiro no caixa eletronico. ") #esta identado 
+
+    print ("Obrigado por ser nosso cliente, tenha um bom dia! ") # está fora da identacao
+
+
+sacar(1000)
+
